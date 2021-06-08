3bd831cbef6d936328dea21b8ed34d17b54199d8
Jawad Khawaja <devjawkha@gmail.com>
2021-06-08 21:34:51 +0200

    create a very rough layout of the app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Crossword Mini</h1>
+      <h1>The Mini Crossword</h1>
+      <div className='container'>
+        <div className='timer-and-menu'>
+          <div>Timer</div>
+          <div>options</div>
+        </div>
+        <div className='board-and-clues'>
+          <div>
+            <div>active clue row</div>
+            <div>board</div>
+          </div>
+          <div>
+            <div>Across </div>
+            <div>Down</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
