1b70660400c66aad68f9f68da078aba192aad821
Jawad Khawaja <devjawkha@gmail.com>
2021-06-10 10:19:43 +0200

    move puzzles data to a separate data directory

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,8 @@ import Options from './components/Options'
 import ActiveClue from './components/ActiveClue'
 import Board from './components/Board'
 import CluesList from './components/CluesList'
+import { clues, answers } from './data/puzzle-1'
 import './App.css'
-
-const clues = {
-	across: {
-		1: 'Fancy party',
-		5: 'The first item ever sold on it was a broken laser printer',
-		6: 'Like many Zoom call participants',
-		8: 'Chloe who won best picture and best director in 2021',
-		9: 'Round number?',
-	},
-	down: {
-		1: 'Valuable stone',
-		2: 'Brimming with activity',
-		3: 'Turning tool in a woodshop',
-		4: 'Annually',
-		7: 'Well, whoop-de-___!',
-	},
-}
-
-// GALA, EBAY, MUTED, ZHAO, ZERO
-// GEM, ABUZZ, LATHE, AYEAR, DOO
-const answers = [
-	'G',
-	'A',
-	'L',
-	'A',
-	'',
-	'E',
-	'B',
-	'A',
-	'Y',
-	'',
-	'M',
-	'U',
-	'T',
-	'E',
-	'D',
-	'',
-	'Z',
-	'H',
-	'A',
-	'O',
-	'',
-	'Z',
-	'E',
-	'R',
-	'O',
-]
 
 function App() {
 	return (
---
+++ src/data/puzzle-1.tsx
@@ -0,0 +1,46 @@
+export const clues = {
+	across: {
+		1: 'Fancy party',
+		5: 'The first item ever sold on it was a broken laser printer',
+		6: 'Like many Zoom call participants',
+		8: 'Chloe who won best picture and best director in 2021',
+		9: 'Round number?',
+	},
+	down: {
+		1: 'Valuable stone',
+		2: 'Brimming with activity',
+		3: 'Turning tool in a woodshop',
+		4: 'Annually',
+		7: 'Well, whoop-de-___!',
+	},
+}
+
+// GALA, EBAY, MUTED, ZHAO, ZERO
+// GEM, ABUZZ, LATHE, AYEAR, DOO
+export const answers = [
+	'G',
+	'A',
+	'L',
+	'A',
+	'',
+	'E',
+	'B',
+	'A',
+	'Y',
+	'',
+	'M',
+	'U',
+	'T',
+	'E',
+	'D',
+	'',
+	'Z',
+	'H',
+	'A',
+	'O',
+	'',
+	'Z',
+	'E',
+	'R',
+	'O',
+]
