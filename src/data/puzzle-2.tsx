9972c3f3fc8ffb21fdc225facf1aa01d9a0d3e1d
Jawad Khawaja <devjawkha@gmail.com>
2021-06-10 20:32:19 +0200

    add one more puzzle

---
+++ src/data/puzzle-2.tsx
@@ -0,0 +1,46 @@
+export const clues = {
+	across: {
+		1: 'The American and Chinese flags have 55 of them collectively',
+		6: 'Most common TV episode title',
+		7: 'Spanish friend',
+		8: 'Coveted prize in Physics and Chemistry',
+		9: 'Number of #1 albums for Drake',
+	},
+	down: {
+		1: 'Stretch across',
+		2: "Pumbaa's pal in The Lion King",
+		3: "Defendant's cover story",
+		4: 'Seth of Superbad',
+		5: 'Took illegally',
+	},
+}
+
+// STARS, PILOT, AMIGO, NOBEL, NINE
+// SPAN, TIMON, ALIBI, ROGEN, STOLE
+export const answers = [
+	'S',
+	'T',
+	'A',
+	'R',
+	'S',
+	'P',
+	'I',
+	'L',
+	'O',
+	'T',
+	'A',
+	'M',
+	'I',
+	'G',
+	'O',
+	'N',
+	'O',
+	'B',
+	'E',
+	'L',
+	'',
+	'N',
+	'I',
+	'N',
+	'E',
+]
