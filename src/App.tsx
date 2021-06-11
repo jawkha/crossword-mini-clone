e8ae013344ece1e7ce625dd40f9df63bcac4f07c
Jawad Khawaja <devjawkha@gmail.com>
2021-06-11 20:29:12 +0200

    app in broken state - lifted all the methods to App root level

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,36 @@ import './App.css'
 
 function App() {
 	const [state, setState] = React.useState(puzzleData)
+	const [direction, setDirection] = React.useState('across')
+
+	function toggleBetweenAcrossAndDown() {
+		direction === 'across' ? setDirection('down') : setDirection('across')
+	}
+
+	function handleClickInsideSquare(id: number) {
+		const stateCopy = state
+		const elementIndex = stateCopy.findIndex(el => el.id === id)
+		if (stateCopy[elementIndex].isActiveCell) {
+			toggleBetweenAcrossAndDown()
+		}
+
+		if (stateCopy[elementIndex].isActiveCell === false) {
+			stateCopy[elementIndex].isActiveCell = true
+		}
+	}
+
+	function handleChange(e: any) {
+		// setInput(e.target.value.toUpperCase())
+	}
+
+	function handleFocus(e: any) {
+		e.target.select()
+		e.target.style.backgroundColor = 'rgb(252, 215, 49)'
+	}
+
+	function handleBlur(e: any) {
+		e.target.style.backgroundColor = 'white'
+	}
 
 	return (
 		<div className='app'>
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -6,29 +6,14 @@ interface ISquareProps {
 }
 
 export default function Square(props: ISquareProps) {
-	const [answer, setAnswer] = React.useState(props.letter)
-	const [input, setInput] = React.useState('')
-
-	function handleChange(e: any) {
-		setInput(e.target.value.toUpperCase())
-	}
-
-	function handleFocus(e: any) {
-		e.target.select()
-		e.target.style.backgroundColor = 'rgb(252, 215, 49)'
-	}
-
-	function handleBlur(e: any) {
-		e.target.style.backgroundColor = 'white'
-	}
-
 	return (
 		<input
-			value={input}
+			type='text'
+			value=''
 			maxLength={1}
-			onFocus={handleFocus}
-			onBlur={handleBlur}
-			onChange={handleChange}
+			onFocus={() => {}}
+			onBlur={() => {}}
+			onChange={() => {}}
 			className={props.letter ? styles.square : styles.empty}
 		/>
 	)
