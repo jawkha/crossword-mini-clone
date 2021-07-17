f0a4173db8d0c22f7314c439d2d1289e17ac1eb5
Jawad Khawaja <devjawkha@gmail.com>
2021-07-17 17:46:00 +0200

    replace onInput with onKeyUp to handle user input - work-in-process

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -24,6 +24,61 @@ export default function Square({
 		isActive ? handleClickInsideActiveSquare() : handleClickInsideInactiveSquare(e, squareData)
 
 	const handleChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		// handle cases where the keypress is an a key which should have no output
+		const inactiveKeys = [
+			'Shift',
+			'Enter',
+			'Tab',
+			'CapsLock',
+			'Control',
+			'Alt',
+			'Meta',
+			'\\',
+			'[',
+			']',
+			"'",
+			';',
+			'/',
+			'.',
+			',',
+			'`',
+			'-',
+			'=',
+		]
+
+		if (inactiveKeys.includes(e.key)) {
+			console.log('inactive key pressed')
+			console.log(e.key)
+			return
+		}
+		// handle cases where the keypress is an arrow key or a spacebar
+		const specialKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Backspace', ' ']
+		if (specialKeys.includes(e.key)) {
+			console.log('special key pressed')
+			// perhaps use a switch statement to account for each key
+			switch (e.key) {
+				case 'ArrowRight':
+					console.log(e.key)
+					return
+				case 'ArrowLeft':
+					console.log(e.key)
+					return
+				case 'ArrowUp':
+					console.log(e.key)
+					return
+				case 'ArrowDown':
+					console.log(e.key)
+					return
+				case 'Backspace':
+					console.log(e.key)
+					return
+				case ' ':
+					console.log(e.key)
+					return
+			}
+		}
+		console.log(e)
+		// handle cases where a character is input into the field
 		inputUserGuess(e)
 		convertNextSquareToActiveSquare()
 	}
@@ -39,7 +94,8 @@ export default function Square({
 				disabled={!squareData.answer}
 				autoFocus={isActive}
 				onClick={handleClick}
-				onInput={handleChange}
+				// onInput={handleChange}
+				onKeyUp={handleChange}
 				className={`${styles.input} ${isActive ? styles.active : ''} ${
 					squareData.answer ? styles.fillable : styles.unfillable
 				} ${
